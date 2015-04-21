adb671e8a1c74446eaf6d75a5a00d6f93c435f4f
Mexinarco <suppordcraft@gmail.com>
2015-04-21 15:42:15 -0500

    Create nf.java

---
+++ Mensajes/nf.java
@@ -0,0 +1,10 @@
+import javax.swing.JApplet;
+import java.awt.Graphics;
+ 
+public class Hello extends JApplet {
+ 
+       public void paint(Graphics g) {(
+        g.drawString(65, 95).getNotification().getString());
+    }
+ 
+}
