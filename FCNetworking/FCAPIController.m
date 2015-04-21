bf963d94db3fdc87631b716cdd18dbc4c80361ae
Ruben <rbnvllgs@gmail.com>
2015-04-21 15:46:46 -0500

    Update FCAPIController.m

--- FCNetworking/FCAPIController.m
+++ FCNetworking/FCAPIController.m
@@ -1,16 +1,12 @@
 #import "FCAPIController.h"
 #import "Singleton.h"
-#import "FCChatDataStoreManager.h"
 #import "FCBaseChatRequestManager.h"
 
 #import "FCAuthFacebookManager.h"
-#import "FCFacebookManager/FCRequestFacebookManager.h"
 
 @interface FCAPIController()
 @property (nonatomic, strong) FCChatDataStoreManager *chatDataStoreManager;
 @property (nonatomic, strong) FCBaseChatRequestManager *chatRequestManager;
-@property (nonatomic, strong) FCAuthFacebookManager *authFacebookManager;
-@property (nonatomic, strong) FCRequestFacebookManager *requestFacebookManager;
 @end
 
 
